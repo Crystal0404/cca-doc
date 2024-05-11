c2100f9a12b14bc1b6880d538af451db18b29255
Crystal0404 <syh040325@163.com>
2024-05-11 13:41:01 +0800

    test

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://crystal0404.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/cca-doc/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
